85db6ddf67cb18ea9fe93b35ebba3c97ec5758b5
svantetic <arkadiusm@gmail.com>
2017-06-23 11:33:09 +0200

    moved cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,17 +3,65 @@
  */
 import React, { Component } from 'react';
 import {connect} from "react-redux";
-class Cart extends React.Component {
+class Cart extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isCartOpened: false
+        };
+        this.orderPrice = this.orderPrice.bind(this);
+        this.deleteProduct = this.deleteProduct.bind(this);
+    }
+    orderPrice() {
+        return this.props.cartProducts.reduce((totalPrice, currentProduct) => {
+            return totalPrice + currentProduct.totalPrice;
+        }, 0);
+    }
+
+    toggleCartStatus() {
+        this.setState({
+            isCartOpened: !this.state.isCartOpened
+        })
+    }
+
+
+    deleteProduct(productKey) {
+
+        this.props.onDeleteProduct({ productKey });
+
+    }
+    //{"product__add-to-cart card-footer-item button is-primary" + (this.isOutOfStock() ? 'button' : 'button is-primary')  + ""}
     render() {
         return (
-            <div className="cart">
-                {this.props.cartProducts.map((singleProduct) =>
-                    <div className="sd">
-                        <p> {singleProduct.name }</p>
-                        <p> {singleProduct.price} </p>
-                    </div>
-                )}
-            </div>
+            <aside className={"cart menu " + (this.state.isCartOpened ? 'cart__is-open' : '')}>
+                <button onClick={this.toggleCartStatus.bind(this)} className="cart__switcher button is-info">
+                    {this.state.isCartOpened ? 'Close' : 'Open Your Cart'}
+                </button>
+                <h3 className="title is-2"> Your order </h3>
+                <h5 className="tag">
+                    Order price: 		{ this.orderPrice() }$
+                </h5>
+                <p>Your products:</p>
+                <div className="cart__products-list" >
+                    {this.props.cartProducts.map((product, key) =>
+                        <div key={key} className="cart__product">
+                            <h5 className="product__title">{ product.name } </h5>
+                            {/*<p className="product__price">Price { product.price }$ </p>*/}
+                            <p className="product__quantity">Quantity: { product.quantity } </p>
+                            <p className="product__total-price"> Total price: { product.totalPrice }$ </p>
+                            <div className="block" >
+								<span className="tag is-danger">
+									Delete
+							<button onClick={this.deleteProduct.bind(this, key)} className="delete is-small"></button>
+							</span>
+                            </div>
+                        </div>
+
+                    )}
+                </div>
+                <button className="button is-danger"> Order! </button>
+
+            </aside>
         )
     }
 }
